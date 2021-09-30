1ad4b47ed8c95ed6e889ff46cd68b3c3b8af3cb2
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-09-30 22:12:06 +0900

    [fixed]view

--- view/register.php
+++ view/register.php
@@ -36,13 +36,7 @@
                 <a class="d-block text-center" href="login_form.php">ログインページへ</a>    
             </div>
             
-        </header>
-        <footer class="footer"><!-- フッター -->
-          <div class="container">
-            <p class="text-muted text-center">&copy; CodeCamp</p>
-          </div>
-        </footer>
-        
+        </header>   
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
     </body>
--- view/user_list.php
+++ view/user_list.php
@@ -48,14 +48,6 @@
             
             <a href="mypage.php">マイページへ</a>
         </main>
-        <footer class="footer"><!-- フッター -->
-          <div class="container">
-            <p class="text-muted text-center">&copy; CodeCamp</p>
-          </div>
-        </footer>
-        
-        
-        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
     </body>
